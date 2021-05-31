bab4dd88be6364f8962637ad5c9f0eaa14debc2c
SRKRaju <srk.raju@icloud.com>
2021-05-31 22:35:27 +0530

    Final_Concept_Added

---
+++ FinalMethods/src/App.java
@@ -0,0 +1,14 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		B obj1 = new B();
+		System.out.println(obj1.getX());
+		obj1.India();
+		obj1.USA();
+	
+	}
+
+}
