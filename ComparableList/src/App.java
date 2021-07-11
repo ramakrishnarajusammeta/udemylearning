786f68f0b8ec1fed0f5f5e76712cb4195a7c2d7b
SRKRaju <srk.raju@icloud.com>
2021-07-11 22:55:45 +0530

    Lists_comparable

---
+++ ComparableList/src/App.java
@@ -0,0 +1,56 @@
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+class Names implements Comparable<Names> {
+	private String name;
+
+	public Names(String name) {
+		super();
+		this.name = name;
+	}
+
+	@Override
+	public int compareTo(Names obj) {
+		if (name.length() == obj.name.length())
+			return 0;
+		else if (name.length() == obj.name.length())
+			return -1;
+		else 
+			return 1;
+	}
+	
+	@Override
+	public String toString() {
+		return name;
+	}
+	
+	
+	
+}
+public class App {
+
+	public static void main(String[] args) {
+		List<Names> names = new LinkedList<Names>();
+		names.add (new Names("Chand"));
+		names.add (new Names("Harini"));
+		names.add (new Names("Swathy"));
+		names.add (new Names("Jaswanth"));
+		
+		App app = new App();
+		app.printList(names);
+		
+		Collections.sort(names);
+		System.out.println("*********");
+		app.printList(names);
+	}
+	
+	void printList(List <Names> list) {
+		ListIterator<Names> iterator =  list.listIterator();
+		while (iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+	}
+
+}
--- LinkedListss/src/App.java
+++ LinkedListss/src/App.java
@@ -1,20 +1,68 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
 
 public class App {
 
 	public static void main(String[] args) {
-		LinkedList<String> countries = new LinkedList<>();
+		//LinkedList<String> countries = new LinkedList<>();
+		// OR
+		List<String> countries = new LinkedList<String>();
 		countries.add("India");
 		countries.add("USA");
+		countries.add("Srilanka");
+		countries.add("Brazil");
 		
 		System.out.println(countries);
 		
 		countries.remove(0);
 		System.out.println(countries);
+		System.out.println("****************");
 		
 		for (String element : countries) {
 			System.out.println("Element : " + element);
 		}
+		
+		// OR
+		System.out.println("****************");
+		new App().printList(countries);
+		
+		
+		// using 'List' interface in 'printList' 
+		//ArrayList<String> names = new ArrayList<>();
+		// OR
+		List<String> names = new LinkedList<String>();
+		
+		names.add("srkraju");
+		names.add("swathy");
+		names.add("harini");
+		System.out.println("****************");
+		//new App().printListIt(names);
+		
+		App app = new App();
+		app.printList(countries);
+		Collections.sort(countries);
+		System.out.println("****************");
+		app.printList(countries);
+		
+	}
+	
+	void printList(List<String> list) {
+		for (String element : list) {
+			System.out.println("Element:" + element);
+		}
+	}
+	
+	void printListIt(List<String> list) {
+		ListIterator<String> iterator =  list.listIterator();
+		while (iterator.hasNext()) {
+			System.out.println("Element:" + iterator.next());
+		}
+		
+		
 	}
 
 }
